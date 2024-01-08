15af0f1e71e3d5a8eace8aeaa07760852f25a4f5
Colin Spangelo <colin.spangelo@gmail.com>
2024-01-08 14:34:00 -0700

    Had to add ml-auto for links to sticky to the right.

--- portfolio/src/Components/BootstrapNavBar.jsx
+++ portfolio/src/Components/BootstrapNavBar.jsx
@@ -8,18 +8,16 @@ import '../Styles/BootstrapNavBar.scss';
 function CollapsibleExample() {
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
-
-        <Navbar.Brand href="/">Colin Spangelo</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="nav-links">
-            <Nav.Link href="/portfolio">Portfolio</Nav.Link>
-            <Nav.Link href="/experience">Experience</Nav.Link>
-            <Nav.Link href="/about">About</Nav.Link>
-            <Nav.Link href="/contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-
+      <Navbar.Brand href="/">Colin Spangelo</Navbar.Brand>
+      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+      <Navbar.Collapse id="responsive-navbar-nav">
+        <Nav className="ml-auto nav-links">
+          <Nav.Link href="/portfolio">Portfolio</Nav.Link>
+          <Nav.Link href="/experience">Experience</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <Nav.Link href="/contact">Contact</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
