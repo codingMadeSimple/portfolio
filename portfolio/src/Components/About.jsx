4c71bba083fc99bdf8eb190c3db7a1b3d5240b4f
Colin Spangelo <colin.spangelo@gmail.com>
2024-01-09 16:15:28 -0700

    Added some currently working on skills.

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -3,7 +3,10 @@ import React from "react";
 function About(props) {
   return (
   <div>
-text
+    <h1>What I am working on</h1>
+    <div>- React</div>
+    <div>- Node</div>
+    <div>- Express</div>
   </div>
 
 )}
