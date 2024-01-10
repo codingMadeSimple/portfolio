0a1132f95e1c9b3869d472f7cdedeb72721329f4
Colin Spangelo <colin.spangelo@gmail.com>
2024-01-10 12:50:47 -0700

    Removed skills learend from About componenet.

--- portfolio/src/Components/About.jsx
+++ portfolio/src/Components/About.jsx
@@ -3,10 +3,6 @@ import React from "react";
 function About(props) {
   return (
   <div>
-    <h1>What I am working on</h1>
-    <div>- React</div>
-    <div>- Node</div>
-    <div>- Express</div>
   </div>
 
 )}
