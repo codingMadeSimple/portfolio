9a552d2faf6ad6e6fb923d2190984c57a4c6ec93
Colin Spangelo <colin.spangelo@gmail.com>
2024-01-10 12:50:19 -0700

    Added New skills being learned page.

---
+++ portfolio/src/Components/SkillsBeingLearned.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function SkillsBeingLearned(props) {
+  return (
+  <div>
+    <h1>What I am working on</h1>
+    <div>- React</div>
+    <div>- Node</div>
+    <div>- Express</div>
+  </div>
+
+)}
+
+export default SkillsBeingLearned
